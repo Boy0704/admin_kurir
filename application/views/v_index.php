518cb9e9b4276f80e6e168adbaf3cdd21a220338
Boy Kurniawan <boykurniawan123@gmail.com>
2020-09-04 22:51:57 +0700

    up

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -154,7 +154,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2019 <a href="#">G2teknologi</a>.</strong> All rights
+    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="#">G2teknologi</a>.</strong> All rights
     reserved.
   </footer>
 
