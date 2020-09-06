43b86e516a34c5298cee40847461d35e5b2431c4
Boy Kurniawan <boykurniawan123@gmail.com>
2020-09-06 22:27:13 +0700

    perbaiki fcm send

--- application/controllers/Api_kurir.php
+++ application/controllers/Api_kurir.php
@@ -563,7 +563,7 @@ class Api_kurir extends CI_Controller {
 		CURLOPT_CONNECTTIMEOUT => 60, # set connection timeout ke 60 detik, untuk mencegah request gantung saat server mati
 		CURLOPT_TIMEOUT => 60, # set timeout ke 120 detik, untuk mencegah request gantung saat server hang
 		CURLOPT_POST => 1, # set method request menjadi POST
-		CURLOPT_POSTFIELDS => '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge" => "1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}', # attached post data dalam bentuk JSON String,
+		CURLOPT_POSTFIELDS => '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge":"1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}', # attached post data dalam bentuk JSON String,
 		// CURLOPT_VERBOSE => 1, # mode debug
 		// CURLOPT_HEADER => 1, # cetak header
 		CURLOPT_SSL_VERIFYPEER => true  
@@ -579,13 +579,13 @@ class Api_kurir extends CI_Controller {
 		if ($http_code == 200) {
 		  # http code === 200 berarti request sukses (harap pastikan server penerima mengirimkan http_code 200 jika berhasil)
 		  // echo $resp;
-			$send = '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge" => "1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}';
+			$send = '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge":"1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}';
 			$this->db->insert('log_notif', array('log'=>$send,'resp'=>$resp));
 		} else {
 		  # selain itu request gagal (contoh: error 404 page not found)
 		  // echo 'Error HTTP Code : '.$http_code."\n";
 		  // echo $resp;
-			$send = '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge" => "1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}';
+			$send = '{"notification": {"body": "'.$body.'","title": "'.$title.'","sound": "default","badge":"1"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "screen": "'.$screen.'", "status": "done"}, "to": "'.$token.'"}';
 			$this->db->insert('log_notif', array('log'=>$send,'resp'=>$resp));
 		}
 		} else {
