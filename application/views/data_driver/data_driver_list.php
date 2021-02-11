28c1c7e7c484d260ea89ca06a84bf0b4cc48b2e7
Boy Kurniawan <boykurniawan123@gmail.com>
2021-02-11 08:05:50 +0700

    search nama

--- application/views/data_driver/data_driver_list.php
+++ application/views/data_driver/data_driver_list.php
@@ -11,7 +11,7 @@
             <div class="col-md-1 text-right">
             </div>
             <div class="col-md-3 text-right">
-                <form action="<?php echo site_url('data_driver/index'); ?>" class="form-inline" method="get">
+                <!-- <form action="<?php echo site_url('data_driver/index'); ?>" class="form-inline" method="get">
                     <div class="input-group">
                         <input type="text" class="form-control" name="q" value="<?php echo $q; ?>">
                         <span class="input-group-btn">
@@ -26,11 +26,12 @@
                           <button class="btn btn-primary" type="submit">Search</button>
                         </span>
                     </div>
-                </form>
+                </form> -->
             </div>
         </div>
         <div class="table-responsive">
-        <table class="table table-bordered" style="margin-bottom: 10px">
+        <table class="table table-bordered" style="margin-bottom: 10px" id="example1">
+            <thead>
             <tr>
                 <th>No</th>
 		<th>Nama Lengkap</th>
@@ -41,7 +42,10 @@
         <th>Status</th>
 		<th>Status Online</th>
 		<th>Action</th>
+    </thead>
+    <tbody>
             </tr><?php
+            $data_driver_data = $this->db->get('data_driver')->result();
             foreach ($data_driver_data as $data_driver)
             {
                 $dataUserLengkap=$this->db->query("SELECT * FROM users where id_user='$data_driver->id_user'")->row_array();
@@ -95,14 +99,15 @@
                 <?php
             }
             ?>
+            </tbody>
         </table>
         </div>
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-md-6">
                 <a href="#" class="btn btn-primary">Total Record : <?php echo $total_rows ?></a>
 	    </div>
             <div class="col-md-6 text-right">
                 <?php echo $pagination ?>
             </div>
-        </div>
+        </div> -->
     
